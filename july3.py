8b1c8ec18e6d1c8ac55a7717dad50db38d5382f3
Chuks I <chuksi@Chukss-MacBook-Pro.local>
2025-07-19 17:39:47 -0600

    Newes goat

--- july3.py
+++ july3.py
@@ -2,3 +2,6 @@ print ('Hello, World!')
 print ('This is a test file.')
 print ('This file is used to test the git commit command.')
 print ('This is the third line of the file.')
+
+print('I am a new line')
+
